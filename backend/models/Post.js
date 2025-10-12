59d3b551fccc34e7e27566698bcd2a43a7d0b9aa
Rajat <sharmarajat3745@gmail.com>
2025-10-12 19:48:53 +0530

    added post schema

---
+++ backend/models/Post.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+
+const postSchema = new mongoose.Schema({
+    caption: {
+        type: 'String',
+        default: ''
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comments:[{
+       type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment', 
+    }]
+},{timestamps:true})
+
+
+export const Post = mongoose.model('Post', postSchema)
