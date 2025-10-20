e7f2b7f7685f85636a53a78eea3578aba58f08e7
Rajat <sharmarajat3745@gmail.com>
2025-10-20 07:43:11 +0530

    created axios instance with base config

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api/v1`
+export const axiosInstance = axios.create({
+    baseURL:BASE_URL,
+    withCredentials:true,
+    headers:{
+        "Content-Type":'application/json'
+    }
+})
