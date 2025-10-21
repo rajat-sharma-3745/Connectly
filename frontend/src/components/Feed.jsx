696ef667541af68c292ad56990c3d9342584da2c
Rajat <sharmarajat3745@gmail.com>
2025-10-21 08:33:16 +0530

    added Feed Component

---
+++ frontend/src/components/Feed.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Post from './Post'
+import Posts from './Posts'
+
+const Feed = () => {
+  return (
+    <div className='flex-1 my-8 flex flex-col items-center pl-2'>
+       <Posts/>
+    </div>
+  )
+}
+
+export default Feed
